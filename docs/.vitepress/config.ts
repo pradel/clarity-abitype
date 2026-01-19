7e199a72e4ff4f946b8a7e2e6635f5c05a0d834b
Léo Pradel <pradel.leo@gmail.com>
2026-01-19 10:04:51 +0100

    feat: docs website

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vitepress";
+
+export default defineConfig({
+  base: "/clarity-abitype/",
+  title: "clarity-abitype",
+  description: "Strict TypeScript types for Clarity ABIs.",
+  themeConfig: {
+    sidebar: [
+      {
+        text: "Guide",
+        items: [{ text: "Getting Started", link: "/" }],
+      },
+      {
+        text: "Examples",
+        items: [
+          {
+            text: "Clarinet Counter",
+            link: "https://github.com/pradel/clarity-abitype/tree/main/examples/clarinet-counter",
+            target: "_blank",
+          },
+        ],
+      },
+    ],
+    socialLinks: [
+      {
+        icon: "github",
+        link: "https://github.com/pradel/clarity-abitype",
+      },
+    ],
+  },
+});
